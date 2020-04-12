bae470998f3dbf267f92e1a38c11e9f3a11fda5d
cuibowen4 <cuibowen3@jd.com>
2020-04-12 14:27:11 +0800

    JVM内存模型学习

---
+++ src/main/java/memory/MyTest1.java
@@ -0,0 +1,22 @@
+package memory;
+
+/**
+ * 虚拟机栈：Stack Frame 栈帧
+ *
+ * 程序计数器（Program Counter）
+ *
+ * 本地方法栈：主要用于处理本地方法
+ *
+ * 堆（Heap）：JVM挂你的最大一块内存空间
+ *
+ * 方法区（Method Area）:存储元信息。永久代（Permanent Generation），从1.8开始，已经彻底废弃了永久代，使用源空间（meta space）
+ *
+ * 运行时常量池：方法区的一部分内容
+ *
+ * 直接内存：Direct Memory
+ **/
+public class MyTest1 {
+
+
+
+}
